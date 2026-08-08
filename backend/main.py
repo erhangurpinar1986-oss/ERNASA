583b01ecd08608f03527c08c1ec7bce81d54c06b
Erhan Gürpınar <GITHUB_HESABINDA_KULLANDIIN_GMAIL_ADRES>
2026-08-08 19:12:41 +0300

    CORS ayarlari guncellendi

--- backend/main.py
+++ backend/main.py
@@ -15,10 +15,12 @@ app = FastAPI(
 initialize_database()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
-    ],
+allow_origins=[
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "https://ernasa.com",
+    "https://www.ernasa.com",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
